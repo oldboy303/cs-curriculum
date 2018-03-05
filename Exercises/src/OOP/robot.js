71e6cb15abd6781164c59baf4cc547ea617244b1
San Guyer <sanguyer@gmail.com>
2018-03-05 13:35:35 -0500

    Delete and do again

--- Exercises/src/OOP/robot.js
+++ Exercises/src/OOP/robot.js
@@ -1,6 +1,31 @@
 class Robot {
 
   constructor (energy) {
+    this.coordinates = [0, 0]
+    this.energy = energy;
+    this.movements = [];
+  }
+
+  position() {
+    return this.coordinates;
+  }
+
+  battery() {
+    return this.energy;
+  }
+
+  moveUp() {
+    this.coordinates[1]++;
+    this.movements.push('up');
+  }
+
+  moveLeft() {
+    this.coordinates[0]--;
+    this.movements.push('left');
+  }
+
+  history() {
+    return this.movements;
   }
 
 }
