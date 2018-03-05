5f53e03eaa0059d3a33b1d3636a9557f37cf2c16
San Guyer <sanguyer@gmail.com>
2018-03-04 20:57:56 -0500

    Editor

--- Exercises/src/OOP/editor.js
+++ Exercises/src/OOP/editor.js
@@ -1,3 +1,27 @@
 module.exports = class Editor {
+    constructor() {
+        this.units = [];
+        this.undos = [];
+    }
+
+    write(text) {
+        this.units.push({ text });
+    }
+
+    toString() {
+        return this.units.reduce((a, b) => b.oV ? a.split(b.oV).join(b.nV) : a + b.text, '')
+    }
+
+    undo() {
+       this.undos.push(this.units.pop());
+    }
+
+    redo() {
+        this.units.push(this.undos.pop());
+    }
+
+    replace(oV, nV) {
+        this.units.push({ oV, nV });
+    }
 
 }
