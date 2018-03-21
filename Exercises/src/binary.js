d0742dee4c5b1b0cded642cf5c051271593a633e
San Guyer <sanguyer@gmail.com>
2018-03-21 17:17:20 -0400

    Binary conversion

--- Exercises/src/binary.js
+++ Exercises/src/binary.js
@@ -13,5 +13,12 @@ You must not use `toString` or `parseInt` or any other
 built-in method in javascript that could aid in the conversion.
 
 */
-module.exports = function(str) {
+module.exports = function( str ) {
+  let result = 0;
+  let digit = 1;
+  for ( let i = str.length - 1; i >= 0; i-- ) {
+    if ( str[i] === '1' ) result += digit;
+    digit *= 2;
+  }
+  return result;
 }
